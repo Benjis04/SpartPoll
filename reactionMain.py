83e610b408180b0b043b152b7dafc1b4d71faedd
Benjis04 <81476154+Benjis04@users.noreply.github.com>
2021-03-27 18:01:13 +0100

    Update reactionMain.py

--- reactionMain.py
+++ reactionMain.py
@@ -6,13 +6,30 @@ bot = commands.Bot(command_prefix = '+', description = '')
 
 @bot.event
 async def on_ready():
-	print('Pret !')
+	print('Pret !\n')
+	print("-----------")
 
 @bot.event
 async def on_message(message):
-	#print("Message dans :", message.channel)
-	await message.add_reaction("✅")
-	await message.add_reaction("❌")
+	#Un message a été détecté
+	print("\nMessage de", message.author, "dans", message.channel, "qui contient '", message.content, "'")
+	
+	#Fonction pour tester si le message est bien dans le salon approprié (ex : suggestion). Si oui la fonction retourne 1 sinon 0.
+	def check(message):
+		messageChannel = message.channel
+		return str(messageChannel) == 'NOM DU SALON POUR LES REACTIONS' #or str(messageChannel) == <Autre channel du serveur à ajouter des réactions>
+	
+	#Phase de test grace à la fonction check crée aupparavant
+	test = check(message)
+	if test == 1:
+		print(test, "; Bon Channel --> Le bot a ajouté les réactions\n")
+		print("-----------")
+		await message.add_reaction("✅")
+		await message.add_reaction("❌")
+	else:
+		print(test, "; Mauvais channel --> Le bot n'a pas ajouté les réactions\n")
+		print("-----------")
+		return
 
 		
-bot.run('')
+bot.run('VOTRE TOKEN ICI')
